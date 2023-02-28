cecb5915a182e5036f1affa233f9727295385e30
Hinko Kocevar <hinkocevar@gmail.com>
2023-02-28 20:55:21 +0100

    wip on generator

--- tests/generate1.py
+++ tests/generate1.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 import pyarrow as pa
 from pyarrow import parquet
 import argparse
+import itertools
 
 
 # This script will generate parquet files containing defined amount
@@ -36,7 +37,7 @@ def generate_pv_names(filename):
     # take some random words to be used as pv parameters
     # first line in the file is CSV header: 'a'
     raw = pd.read_csv(wordlist)
-    # get the words that are between 4 and 10 chars long
+    # get the words that are between 4 and 9 chars long
     mask = (raw['a'].str.len() > 3) & (raw['a'].str.len() < 10)
     raw = raw.loc[mask]
     params_all = raw['a'].to_list()
@@ -44,7 +45,7 @@ def generate_pv_names(filename):
     # take ess prefixes
     raw = pd.read_csv(ess_wordlist)
     # get the words that are longer than 8
-    mask = (raw['a'].str.len() > 8)
+    mask = (raw['a'].str.len() > 12)
     raw = raw.loc[mask]
     prefixes_all = raw['a'].to_list()
 
@@ -52,24 +53,28 @@ def generate_pv_names(filename):
     random.shuffle(params_all)
 
     # take 50 prefixes; system names
-    prefixes = random.choices(prefixes_all, k=50)
+    prefixes = random.sample(prefixes_all, k=50)
     # take 1000 parameters; pv attributes
-    params = random.choices(params_all, k=1000)
+    params = random.sample(params_all, k=1000)
 
     prefix_series = pd.Series(prefixes)
-    pv_series = pd.Series([], dtype='str')
-    for i in range(0, 1000):
-        param_series = pd.Series(random.choices(params, k=50))
-        pv_series = pd.concat([pv_series, prefix_series + ':' + param_series], ignore_index=True)
+    param_series = pd.Series(params)
 
-    # print(pv_series)
-    # print('len:', len(pv_series))
+    pvs = [r[0] + ':' + r[1] for r in itertools.product(prefixes, params)]
+    random.shuffle(pvs)
 
-    with open(filename, 'w') as fp:
-        json.dump(pv_series.to_list(), fp, indent=2)
+    # final list of pvs
+    pv_series = pd.Series(pvs, dtype='str')
+    if pv_series.value_counts().max() == 1:
+        with open(filename, 'w') as fp:
+            json.dump(pvs, fp, indent=2)
 
-    print('filename', filename, 'created')
-    return pv_series.to_list()
+        print('filename', filename, 'created')
+        return pvs
+    else:
+        print('pv_series value count max is not 1 !!!')
+        print('pv_series counts:\n', pv_series.value_counts())
+        return None
 
 
 def task1(work, id):
@@ -78,54 +83,53 @@ def task1(work, id):
     ts = work['ts']
     num = len(work['pvs'])
     filename = work['filestub'] % id
-    final_rows = work['rows_per_day'] * work['days']
+    final_rows = work['event_count'] * work['day_count']
 
     writer = parquet.ParquetWriter(filename, schema=work['schema'], version='2.6')
     pvnames = []
     timestamps = []
     integers = []
-    
-    total_rows = 0
-    day_rows = 0
+
+    num_events = 0
+    num_days = 0
     start_time = perf_counter()
-    while True:
+
+    while num_days < work['day_count']:
         timestamps += num * [ts]
         tss = int(datetime.timestamp(ts)*1e6)
         integers += [tss + n for n in range(num)]
 
-        day_rows += num
-        total_rows += num
-
-        # next time slice
+        # next event / time slice
         ts = ts + timedelta(milliseconds=72)
+        num_events += 1
 
-        # print('\rthread %d %d / %d' % (id, total_rows, final_rows), end='')
+        if (num_events % 1000) == 0:
+            print('\rthread %d event %d / %d' % (id, num_events , work['event_count']), end='')
 
-        if day_rows >= work['rows_per_day']:
-            print('\rthread %d WRITING %d / %d' % (id, total_rows, final_rows))
-            pvnames = int(day_rows / num) * work['pvs']
+        if num_events == work['event_count']:
+            print('\rthread %d event %d / %d' % (id, num_events , work['event_count']))
+            num_days += 1
+            print('\rthread %d WRITING day %d / %d                 ' % (id, num_days, work['day_count']))
+            pvnames = num_events * work['pvs']
             table = pa.table([pvnames, timestamps, integers], schema=work['schema'])
             # table.sort_by('timestamp')
             writer.write(table)
             timestamps = []
             integers = []
-            day_rows = 0
+            num_events = 0
 
-        if total_rows >= final_rows:
-            break
-
-    print('\rthread %d DONE %d / %d' % (id, total_rows, final_rows))
+    print('\rthread %d DONE day %d / %d                 ' % (id, num_days, work['day_count']))
     end_time = perf_counter()
     print('thread %d took %.2f second(s) to complete.' % (id, end_time - start_time))
 
     writer.close()
 
 
-def work1(pvs, nthreads=1, npvs=200, ndays=1):
+def work1(pvs, args):
     work_name = 'work1'
     start_date = '2023-02-11'
-    work = [None] * nthreads
-    threads = [None] * nthreads
+    work = [None] * args.threads
+    threads = [None] * args.threads
 
     schema = pa.schema([
         ('pvname', pa.string()),
@@ -139,16 +143,15 @@ def work1(pvs, nthreads=1, npvs=200, ndays=1):
 
     # start the threads
     for n in range(len(threads)):
-        s = n * npvs
-        e = (n + 1) * npvs
+        s = n * args.pv_count
+        e = (n + 1) * args.pv_count
         w = {
             'ts': datetime.fromisoformat(start_date),
             'pvs': pvs[s:e],
             'filestub': work_name+'-%d.parquet',
             'schema': schema,
-            # 14 * 60 * 60 * 24 = 1209600 entries in a day @ 14 Hz
-            'rows_per_day': 14 * 60 * 60 * 24,
-            'days': ndays
+            'event_count': args.events,
+            'day_count': args.days
         }
         work[n] = w
         threads[n] = Thread(target=task1, args=(w, n))
@@ -166,12 +169,17 @@ parser.add_argument('work', action='store')
 parser.add_argument('-d', '--days', action='store', default='1', type=int)
 parser.add_argument('-p', '--pv_count', action='store', default='200', type=int)
 parser.add_argument('-t', '--threads', action='store', default='1', type=int)
+parser.add_argument('-e', '--events', action='store', default='10000', type=int)
 args = parser.parse_args()
 print('args:', args)
 
 pvs = generate_pv_names('fake-pvs.json')
+if pvs is None:
+    exit(1)
+
 print('have %d fake PVs..' % len(pvs))
+
 if args.work == 'work1':
-    work1(pvs, nthreads=args.threads, npvs=args.pv_count, ndays=args.days)
+    work1(pvs, args)
 else:
     print('work function %s not found!' % args.work)
